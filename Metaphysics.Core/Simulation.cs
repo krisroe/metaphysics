8780735bb18267535dfb1984ecf833190a0ffee8
Chris Rowe <destroyallpugs@gmail,com>
2026-03-29 20:09:21 -0500

    add simulation resources

--- Metaphysics.Core/Simulation.cs
+++ Metaphysics.Core/Simulation.cs
@@ -3,10 +3,20 @@
 public class Simulation : IDisposable
 {
     private bool _disposed = false;
+    private readonly List<SimulationResource> _resources = new();
 
-    public Simulation()
+    public Simulation? Parent { get; }
+    public IReadOnlyList<SimulationResource> Resources => _resources;
+
+    public Simulation(Simulation? parent = null)
     {
+        Parent = parent;
         Console.WriteLine("Simulation beginning...");
+    }
+
+    public void AddResource(SimulationResource resource)
+    {
+        _resources.Add(resource);
     }
 
     public void Dispose()
