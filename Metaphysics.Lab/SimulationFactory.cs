2bd75d3b074a05fe5adb2d7124dbcbc9c924c37c
Chris Rowe <destroyallpugs@gmail,com>
2026-04-11 10:05:37 -0500

    add life agent to simulation maturation function

--- Metaphysics.Lab/SimulationFactory.cs
+++ Metaphysics.Lab/SimulationFactory.cs
@@ -22,13 +22,28 @@ public static class SimulationFactory
 
         //add a general observer
         mapping = new Dictionary<SimulationEntity, SimulationEntity?>();
-        entity = new SimulationEntity("General Observer")
+        SimulationEntity observerEntity = new SimulationEntity("General Observer")
         {
             IsObserver = true,
         };
-        entity.Resources.Add(new SimulationResource(ResourceType.MetaphysicalEnergy, 1, false));
-        newEntities = new List<SimulationEntity>() { entity };
+        observerEntity.Resources.Add(new SimulationResource(ResourceType.MetaphysicalEnergy, 1, false));
+        newEntities = new List<SimulationEntity>() { observerEntity };
         simulation.AddOrChangeEntities(mapping, newEntities, simulation);
+
+        Console.WriteLine("Lots of physics stuff happens in the simulation.");
+
+        //At some point an agent is observed capable of advancing life, timeline is imprecise.
+        //~4.4–4.3 billion years ago: could support life but this is not widely accepted as when life actually began.
+        //~4.1–3.8 billion years ago: there could be oceans, hydrothermal vents, subsurface refuges for life despite late heavy bombardment
+        //By 3.8 billion years ago, life was definitely present.
+        mapping = new Dictionary<SimulationEntity, SimulationEntity?>();
+        entity = new SimulationEntity("Life Agent")
+        {
+            IsAgent = true,
+        };
+        SimulationEntity newObserver = new SimulationEntity(observerEntity);
+        newObserver.Resources.Add(new SimulationResource(ResourceType.MetaphysicalEnergy, 1, true));
+        mapping[observerEntity] = newObserver;
 
         //Lots of stuff happens in the simulation, but the relevant milestone is the first life forms.
         //Timeline for abiogeneis is 3.8 to 3.5 billion years ago, but possibly back to 4.1 billion years ago.
