fdc4d35eecc9f5f5bb922b9f049002710a48ec29
Chris Rowe <destroyallpugs@gmail,com>
2026-04-11 15:02:58 -0500

    abiogenesis update

--- Metaphysics.Lab/SimulationFactory.cs
+++ Metaphysics.Lab/SimulationFactory.cs
@@ -34,13 +34,14 @@ public static class SimulationFactory
         //~4.4–4.3 billion years ago: could support life but this is not widely accepted as when life actually began.
         //~4.1–3.8 billion years ago: there could be oceans, hydrothermal vents, subsurface refuges for life despite late heavy bombardment
         //By 3.8 billion years ago, life was definitely present.
+        Console.WriteLine("A life agent is observed.");
         mapping = new Dictionary<SimulationEntity, SimulationEntity?>();
-        entity = new SimulationEntity("Life Agent")
+        SimulationEntity lifeAgentEntity = new SimulationEntity("Life Agent")
         {
             IsAgent = true,
         };
-        SimulationEntity newObserver = CloneAndAddValueAddResource(observerEntity, mapping);
-        simulation.AddOrChangeEntities(mapping, [entity], simulation);
+        observerEntity = CloneAndAddValueAddResource(observerEntity, mapping);
+        simulation.AddOrChangeEntities(mapping, [lifeAgentEntity], simulation);
 
         //Lots of stuff happens in the simulation, but the relevant milestone is the first life forms.
         //Timeline for abiogeneis is 3.8 to 3.5 billion years ago, but possibly back to 4.1 billion years ago.
@@ -48,23 +49,11 @@ public static class SimulationFactory
         //2. There are competing theories of abiogenesis, which is correct is not really relevant here.
         //2a. Heterotrophs first (using organic molecules in the environment as a resource), chemoautotrophy develops later
         //2b. Life begins as metabolic chemoautotrophy capturing naturally occuring reactions into pathway such as the acetyl-CoA pathway
-        Console.WriteLine("A group of living organisms comes into existence with chemoautotrophy.");
-        SimulationEntity organisms = new SimulationEntity("Primitive prokaryotic unicellular organisms with chemoautotrophy");
-        simulation.AddOrChangeEntities(new Dictionary<SimulationEntity, SimulationEntity?>(), [organisms], simulation);
-
-        Console.WriteLine("This group of living organisms has staying power.");
-        var nextIteration = new SimulationEntity(organisms);
-        nextIteration.Resources.Add(new SimulationResource(ResourceType.MetaphysicalEnergy, 1m, true));
-        simulation.AddOrChangeEntities(new Dictionary<SimulationEntity, SimulationEntity?> { { organisms, nextIteration } }, [], simulation);
-
-        Console.WriteLine("More similar organisms come into existence.");
-        organisms = new SimulationEntity("Primitive prokaryotic unicellular organisms with chemoautotrophy");
-        simulation.AddOrChangeEntities(new Dictionary<SimulationEntity, SimulationEntity?>(), [organisms], simulation);
-
-        Console.WriteLine("The initial group of living organisms all die or divide, allowing their resources to be harvested by the simulation.");
-        var deceasedFirstOrganisms = new SimulationEntity(nextIteration);
-        deceasedFirstOrganisms.Status = SimulationEntityStatus.Deceased;
-        simulation.AddOrChangeEntities(new Dictionary<SimulationEntity, SimulationEntity?> { { nextIteration, deceasedFirstOrganisms } }, [], simulation);
+        Console.WriteLine("Prokaryotic unicellular organisms with chemoautotrophy come into existence.");
+        mapping = new Dictionary<SimulationEntity, SimulationEntity?>();
+        entity = new SimulationEntity("Prokaryotic unicellular organisms with chemoautotrophy");
+        lifeAgentEntity = CloneAndAddValueAddResource(lifeAgentEntity, mapping);
+        simulation.AddOrChangeEntities(mapping, [entity], simulation);
 
         return simulation;
     }
